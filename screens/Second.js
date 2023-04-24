418066aed7be04da1ed74103d89567dd424d67fb
Danish Jamal <mddanishjamal04@yahoo.com>
2023-04-24 16:39:37 +0530

    added login page

---
+++ screens/Second.js
@@ -0,0 +1,12 @@
+import { View, Text,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Second = () => {
+  return (
+    <SafeAreaView className='mt-9'>
+        <Text>This is Second Screen</Text>        
+    </SafeAreaView>
+  )
+}
+
+export default Second
